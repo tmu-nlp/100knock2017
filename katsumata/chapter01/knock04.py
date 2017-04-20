32042844a6d431489ceb21face40ad695ae432bb
勝又智 <satoru@katsumatasatoshinoMacBook-Air.local>
2017-04-20 22:48:11 +0900

    knock04

--- katsumata/chapter01/knock04.py
+++ katsumata/chapter01/knock04.py
@@ -3,7 +3,7 @@ str1 = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might A
 dic = {}
 str2 = str1.split()
 print (str2)
-#リストから1要素ずつ持ってきてリストの単語頭1or2文字とタプルを作成、appendしていく
+#リストから1要素ずつ持ってきて、その要素と出現順を辞書に逐次追加していく
 count = 1
 for c in str2:
     if count == 1 or count == 5 or count == 6 or count == 7 or count == 8 or count == 9 or count == 15 or count == 16 or count == 19 :
