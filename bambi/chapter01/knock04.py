8d08ac8f07a8316b9ec162e3b1c472348929eb27
mingpossible <mingchanbambina@gmail.com>
2017-04-24 16:41:53 +0900

    chapter1にコンテンツを追加

---
+++ bambi/chapter01/knock04.py
@@ -0,0 +1,11 @@
+content = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+words = content.split()
+result_dict = {}
+for index, word in enumerate(words):
+    key = ""
+    if index % 2 == 0:
+        key = word[:2]
+    else:
+        key = word[:1]
+    result_dict[key] = i
+print (result_dict)
