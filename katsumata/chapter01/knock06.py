0349cb69229c9773f11ae400b46943a8bd28a468
勝又智 <satoru@katsumatasatoshinoMacBook-Air.local>
2017-04-25 22:30:45 +0900

    small change

--- katsumata/chapter01/knock06.py
+++ katsumata/chapter01/knock06.py
@@ -1,11 +1,12 @@
 #単語n-gram
+"""
 def word_n_gram(n, str1):
     list_w_ngram = []
     str2 = str1.split()
     for i in range(len(str2)-(n-1)):
         list_w_ngram.append(str2[i:i+n])
     return list_w_ngram
-
+"""
 #文字n-gram
 def char_n_gram(n, str1):
     list_char_ngram = []
@@ -32,7 +33,24 @@ print (X - Y)
 print ('差集合Y-X')
 print (Y - X)
 
+
 if 'se' in X and 'se' in Y:
-    print ('inculude "se"')
+    print ('include "se"')
 else:
     print ('not include "se"')
+
+
+"""
+if 'se' in X:
+    print ('include "se" in X')
+    flag_X = 1
+else:
+    flag_X = 0
+if 'se' in Y:
+    print ('include "se" in Y')
+    flag_Y = 1
+else:
+    flag_Y = 0
+if flag_X == 0 and flag_Y == 0:
+    print ('not include')
+"""
