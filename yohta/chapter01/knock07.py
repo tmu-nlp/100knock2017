47b536f4693b2b96417757c0c9482f7c6c62f2ad
長内洋太 <yo-ta@osanaiyouta-no-MacBook-Pro.local>
2017-04-27 20:47:32 +0900

    update

--- yohta/chapter01/knock07.py
+++ yohta/chapter01/knock07.py
@@ -1,4 +1,5 @@
 def string1(x,y,z):
-    str1 = ("%d時の%sは%.1f" % (x,y,z))
+#    str1 = ("%d時の%sは%.1f" % (x,y,z))
+    str1 = ("{0}時の{1}は{2}".format(x,y,z))
     return str1
 print(string1(12,"気温",22.4))
