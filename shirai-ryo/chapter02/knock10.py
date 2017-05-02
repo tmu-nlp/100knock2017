15b81fd663b42f9faaa0ae8a510e61f06487d1f0
shirai ryosuke <shirairyosuke@sd.tmu.ac.jp>
2017-05-02 14:10:28 +0900

    up

--- shirai-ryo/chapter02/knock10.py
+++ shirai-ryo/chapter02/knock10.py
@@ -1,6 +1,11 @@
-f = open('hightemp.txt')
-lines = f.read()
+f = open('hightemp.txt', 'r')
+count = 0
+for line in f:
+    count += 1
 
-print(len(lines))
+print(count)
+f.close()
+
 
 #UNIXよくわからないので後で追加する
+#wc -l hightemp.txt
