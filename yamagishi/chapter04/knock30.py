6451f614b0e1a22b0884600498770a68077ba7fe
yamagishi <pfm.harg.0524@gmail.com>
2017-05-10 15:40:30 +0900

    chapter04

---
+++ yamagishi/chapter04/knock30.py
@@ -0,0 +1,16 @@
+def get_morphs(file_name):
+    sentence = list()
+    for line in open(file_name):
+        if line.startswith('EOS'):
+            yield sentence
+            sentence = list()
+        else:
+            mecab_line = line.rstrip('\n').split('\t')
+            morph = mecab_line[-1].split(',')
+            word = {'surface': mecab_line[0], 'base': morph[6], 'pos': morph[0], 'pos1': morph[1]}
+            sentence.append(word)
+
+if __name__ == '__main__':
+    for sentence in get_morphs('./neko.txt.mecab'):
+        for word in sentence:
+            print(word)
