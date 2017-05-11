e56fea5378fae54ef432482014a3f1c49dea1533
Michiki Kurosawa <michiki@Michiki-no-MacBook-Pro.local>
2017-05-11 19:08:36 +0900

    25

---
+++ kurosawa/chapter03/knock25.py
@@ -0,0 +1,31 @@
+from knock20 import jsonget
+import re
+
+def kisotemp():
+    en = jsonget().split('\n')
+    old = list()
+    bag = str()
+    dic = dict()
+    flag = 0
+    for data in en:
+        if '基礎情報' in data:
+            flag = 1
+        elif re.match('}}',data):
+            flag = 0
+        elif flag == 1:
+            data = re.sub(r'^\|','',data)
+#            print(data)
+            data = data.split('=')
+            data.append('')
+            if data[1] is '':
+                data[1] = old + ' & ' + data[0]
+                data[0] = bag
+            dic[data[0]] = data[1]
+            bag = data[0]
+            old = data[1]
+#            print(old)
+#    print(dic)
+    return dic
+
+if __name__ == '__main__':
+    print(kisotemp())
