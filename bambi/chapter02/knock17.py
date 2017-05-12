a6d6f6ecffe9d3b80c5b49c463e16de8ab063772
mingpossible <mingchanbambina@gmail.com>
2017-05-12 11:05:41 +0900

    修正

--- bambi/chapter02/knock17.py
+++ bambi/chapter02/knock17.py
@@ -1,7 +1,7 @@
 import os
 with open("hightemp.txt") as file:
     lines = file.readlines()
-    first_column_list = [x.split("\t")[0] for x in lines]
-    print(sorted(set(first_column_list)))
+    first_column_list = set(x.split("\t")[0] for x in lines)
+    print(sorted(first_column_list))
 print("確認")
 os.system("cut -f 1 hightemp.txt | sort | uniq")
