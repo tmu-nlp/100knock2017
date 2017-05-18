c85c7020c2f45714afa0380b4a0aed825cf351a3
mingpossible <mingchanbambina@gmail.com>
2017-05-18 10:11:20 +0900

    修正

--- bambi/chapter03/knock28.py
+++ bambi/chapter03/knock28.py
@@ -1,15 +1,18 @@
-import re
+import re, string
 from collections import OrderedDict
 from knock27 import dataRemoveLinkMarkup
+
 class EnglandContent(object):
     def removeMarkup(self, content):
+        return re.sub('%s' % re.escape(string.punctuation), '', content)
+    '''
         content = re.sub(r'<.*?>', r"", content)
         content = re.sub(r'(==+)', r"", content)
         content = re.sub(r'\{\{|\}\}', r"", content)
         content = re.sub(r'\[|\]', r"", content)
         content = re.sub(r'(#+)|\;|(\*+)', r"", content)
         return content
-    
+    '''
     def getItem(self):
         data = dataRemoveLinkMarkup()
         result = OrderedDict()
@@ -18,7 +21,8 @@ class EnglandContent(object):
             new_k = k.replace(" ", "")
             result[new_k] = new_v
         return result
-            
-            
+
+
 result = EnglandContent().getItem()
+
 print(result)
