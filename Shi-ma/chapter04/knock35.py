919337ae7b6869c655414075348fd5cec2fd7b15
Shimanaka Hiroki <Shimanaka_Hiroki@shima-no-MacBook-Air.local>
2017-05-25 15:17:52 +0900

    04_update

--- Shi-ma/chapter04/knock35.py
+++ Shi-ma/chapter04/knock35.py
@@ -3,15 +3,15 @@ import knock30
 if __name__ == '__main__':
     with open('../data/neko.txt.mecab', 'r') as data_in:
         mecab_data = knock30.mecab_dict_data(data_in)
-    nouns = []
-    for line in mecab_data:
-        nouns_series = ''
-        flag = -99
-        for i, word in enumerate(line):
-            if word['pos'] == '名詞' and (flag == -99 or flag == i-1):
-                nouns_series += word['surface']
-                flag = i
-            elif word['pos'] != '名詞' and flag == i-1 and nouns_series not in nouns:
-                nouns.append(nouns_series)
-    with open('../data/neko_nouns_series.txt', 'w') as data_out:
-        data_out.write(str(nouns))
+        nouns = []
+        for line in mecab_data:
+            nouns_series = ''
+            flag = -99
+            for i, word in enumerate(line):
+                if word['pos'] == '名詞' and (flag == -99 or flag == i-1):
+                    nouns_series += word['surface']
+                    flag = i
+                elif word['pos'] != '名詞' and flag == i-1 and nouns_series not in nouns:
+                    nouns.append(nouns_series)
+        with open('../data/neko_nouns_series.txt', 'w') as data_out:
+            data_out.write(str(nouns))
