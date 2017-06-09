29e97cda7b80b1851afaab0a627e9109a8b17b73
michi96 <kurosawa-michiki@ed.tmu.ac.jp>
2017-06-09 16:17:36 +0900

    Delete knock56.py

--- kurosawa/chapter06/knock56.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as ET
-from knock53 import xml_parse
-
-if __name__ == '__main__':
-    root = xml_parse('nlp.txt.xml')
-    for word_ in list(root.getiterator('token')):
-        if word_.findtext('NER') == 'PERSON':
-            print(word_.findtext('word'))
