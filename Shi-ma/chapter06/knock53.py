8df1962a1a6a607f47d7805c17108fa135053fff
Shimanaka Hiroki <Shimanaka_Hiroki@shima-no-MacBook-Air.local>
2017-06-11 01:21:48 +0900

    except_56

--- Shi-ma/chapter06/knock53.py
+++ Shi-ma/chapter06/knock53.py
@@ -1,14 +1,17 @@
-import re
+import xml.etree.ElementTree as ET
 
-def make_corenlp_word(data_in):
-    pettern = re.compile(r'(?<=<word>).+?(?=</word>)')
-    for line in data_in:
-        word = pettern.search(line)
-        if word:
-            yield word.group()
+def make_corenlp_word(data_in_path, data_out):
+    tree = ET.parse(data_in_path)
+    root = tree.getroot()
+    for sentence in root.iter('sentence'):
+        for token in sentence.iter('token'):
+            print(token.find('word').text, file=data_out)
+        print('', file=data_out)
 
 if __name__ == '__main__':
-    with open('../data/nlp.txt.xml', 'r') as data_in:
-        with open('knock53_result.txt', 'w') as data_out:
-            for word in make_corenlp_word(data_in):
-                print(word, file=data_out)
+    with open('knock53_result.txt', 'w') as data_out:
+        data_in_path = '../data/knock50_result.txt.xml'
+        make_corenlp_word(data_in_path, data_out)
+
+# 普通に nlp.txt を StanfrdCoreNLP で解析するとピリオド区切りになってしまうため、knock50 で作った一行一文形式のファイルを以下のオプションで実行し、一文区切りで解析した。
+# java -cp stanford-corenlp-3.7.0.jar:stanford-corenlp-3.7.0-models.jar:xom.jar:joda-time.jar:slf4j-api.jar:jollyday.jar -Xmx3g edu.stanford.nlp.pipeline.StanfordCoreNLP -ssplit.eolonly -annotators tokenize,ssplit,pos,lemma,ner,parse,dcoref -file ../knock50_result.txt
--- Shi-ma/chapter06/knock55.py
+++ Shi-ma/chapter06/knock55.py
@@ -1,16 +1,14 @@
-import re
+import xml.etree.ElementTree as ET
 
-def make_corenlp_Person(data_in):
-    pettern_word = re.compile(r'(?<=<word>).+?(?=</word>)')
-    pettern_NER = re.compile(r'(?<=<NER>).+?(?=</NER>)')
-    for line in data_in:
-        if pettern_word.search(line):
-            word = pettern_word.search(line).group()
-        if pettern_NER.search(line) and pettern_NER.search(line).group() == 'PERSON':
-            yield word
+def make_corenlp_Person(data_in_path, data_out):
+    tree = ET.parse(data_in_path)
+    root = tree.getroot()
+    for sentence in root.iter('sentence'):
+        for token in sentence.iter('token'):
+            if token.find('NER').text == 'PERSON':
+                print(token.find('word').text, file=data_out)
 
 if __name__ == '__main__':
-    with open('../data/nlp.txt.xml', 'r') as data_in:
-        with open('knock55_result.txt', 'w') as data_out:
-            for word in make_corenlp_Person(data_in):
-                print(word, file=data_out)
+    with open('knock55_result.txt', 'w') as data_out:
+        data_in_path = '../data/knock50_result.txt.xml'
+        make_corenlp_Person(data_in_path, data_out)
