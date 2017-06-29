17a92405e22aabbc66934ef42bf0ad2f8ac9c621
長内洋太 <>
2017-06-29 13:28:54 +0900

    update

--- yohta/chapter08/knock78.py
+++ yohta/chapter08/knock78.py
@@ -30,28 +30,6 @@ def make_list(list_):
 #print(np.shape(pn_list_tr[0]))
 score_list = []
 
-for i in range(k_fold):
-    lr = LogisticRegression()
-    id_train = []
-    pn_train = []
-    word_id_tr = make_list(word_id)
-    pn_list_tr = make_list(pn_list)
-    id_test = word_id_tr[i]
-    pn_test = pn_list_tr[i]
-    del word_id_tr[i]
-    del pn_list_tr[i]
-#        print(len(pn_test))
-    for ii in range(len(word_id_tr)):
-        for jj in range(len(word_id_tr[ii])):
-            id_train.append(word_id_tr[ii][jj])
-            pn_train.append(pn_list_tr[ii][jj])
-#        print(np.shape(id_train),np.shape(pn_train))
-    lr.fit(id_train, pn_train)
-    score_list.append(lr.predict(id_test)[0])
-
-
-
-
 if __name__ == '__main__':
     for i in range(k_fold):
         lr = LogisticRegression()
@@ -69,7 +47,14 @@ if __name__ == '__main__':
                 id_train.append(word_id_tr[ii][jj])
                 pn_train.append(pn_list_tr[ii][jj])
 #        print(np.shape(id_train),np.shape(pn_train))
-        lr.fit(id_train, pn_train)
+        #id_train = list(map(list,zip(*id_train)))
+        #pn_train = list(map(list,zip(pn_train)))
+        lr.fit(id_train,pn_train)
+
+        score_list = []
+        for line in id_test:
+            score_list.append(lr.predict(line)[0])
+
         result = confmat_status(pn_test,score_list)
         print('{}回目:'.format(i+1))
         print('正解率:{}\n適合率:{}\n再現率:{}\nf1_score:{}\n'.format(result[0],result[1],result[2],result[3]))
