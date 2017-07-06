7aa3f50682503360b834807248765c5fea45a361
mingpossible <mingchanbambina@gmail.com>
2017-07-06 18:02:00 +0900

    …

--- bambi/chapter10/knock95.py
+++ bambi/chapter10/knock95.py
@@ -1,14 +1,13 @@
 import scipy.stats
 import pickle
+import numpy as np
 human = []
 data = open("combined.csv").readlines()[1:]
 model = pickle.load(open("similar94.pickle","rb"))
 words = [m[0] for m in model]
-for d in data:
-    d = d.strip("\n").split(",")
-    key = d[0]+"\t"+d[1]
-    if key in words:
-        human.append((key, d[2]))
-human = sorted(human, key = lambda x:x[1], reverse=True)
-model = sorted(model, key = lambda x:x[1], reverse=True)
-print(scipy.stats.spearmanr(human,model))
+model90 = [m.split("\t")[3] for m in model]
+human = [m.split("\t")[2] for m in model]
+model85= [m.split("\t")[4] for m in model]
+
+print(scipy.stats.spearmanr(human,model90, axis=None))
+print(scipy.stats.spearmanr(human,model85, axis=None))#axis=None, then both arrays will be raveled
